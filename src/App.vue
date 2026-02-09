ea09c0394499b3e1a9a5f58d8659a5b7f29f3527
Krzysztof Dabrowski <biz@op.pl>
2026-02-09 22:48:59 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<script setup>
+  import q from "./assets/data/quizes.json"
+  import {ref} from "vue"
+
+  const quizes = ref(q)
+</script>
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text" placeholder="...">
+    </header>
+    <div class="options-container">
+      <div v-for="quiz in quizes" :key="quiz.id">
+        <div class="card">
+          <img :src="quiz.img" alt="">
+          <div class="card-text">
+            <h2>{{ quiz.name }}</h2>
+            <p>{{ quiz.questions.length }} questions</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+header input {
+  border: none;
+  background-color: rgba(128,128,128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+/* Card */
+
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0,0,0,0,0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.card image {
+  width: 100%;
+  height: 190px;
+  margin: 0;
+}
+
+.card text {
+  padding: 0 5px;
+}
+
+.card h2 {
+  font-weight: bold;
+}
+
+/* Options Container */
+
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;;
+}
+
+</style>
